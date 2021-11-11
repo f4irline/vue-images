4660952b263bbd22f1af83442af26289b6aa5aa5
Tommi Lepola <lepola.tommi@gmail.com>
2021-11-11 14:07:46 +0200

    Attempt to fix express routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const history = require('connect-history-api-fallback');
 const path = require('path');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, '../dist')));
+const staticFileMiddleware = express.static(path.join(__dirname, '../dist'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+  index: '/index.html'
+}));
+app.use(staticFileMiddleware);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('Listening on port ' + port);
+  console.log('Listening on port ' + port);
 });
